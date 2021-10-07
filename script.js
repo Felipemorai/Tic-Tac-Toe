f6470de3e8905ff1df56deb75163ae0cb07ce08e
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-07 15:04:31 -0300

    Allbox

--- script.js
+++ script.js
@@ -3,10 +3,20 @@
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
 selectOBtn = selectBox.querySelector(".playerO"),
-playBoard = document.querySelector(".play-board");
+playBoard = document.querySelector(".play-board"),
+allBox = document.querySelectorAll("section span");
 
+/* Window loaded */
 window.onload = () => {
+    for (let i = 0; i < allBox.length; i++) {
+        allBox[i].setAttribute("onclick", "clickedBox(this)");
+    }
+
     selectXBtn.onclick = () => {
+        selectBox.classList.add("hide");
+        playBoard.classList.add("show");
+    }
+    selectOBtn.onclick = () => {
         selectBox.classList.add("hide");
         playBoard.classList.add("show");
     }
