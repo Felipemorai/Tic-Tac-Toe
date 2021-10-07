51eb0d7ea802dfd8e3dbf68fa5092a296b252cb7
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-10-07 14:46:30 -0300

    Xbtn

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 const selectBox = document.querySelector(".select-box"),
 selectXBtn = selectBox.querySelector(".playerX"),
-selectOBtn = selectBox.querySelector(".playerO");
+selectOBtn = selectBox.querySelector(".playerO"),
+playBoard = document.querySelector(".play-board");
 
 window.onload = () => {
     selectXBtn.onclick = () => {
         selectBox.classList.add("hide");
+        playBoard.classList.add("show");
     }
 }
